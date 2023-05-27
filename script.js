7159aef3393377974f11d04edd35d55fc8267b80
dimitrisx26 <dxynos@outlook.com>
2023-05-27 21:39:53 +0300

    the logic to fill the gameboard is created

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    let board = ["", "", "", "", "", "", "", "", ""];
+    let board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
     const getBoard = () => board;
     const setBoard = (i, value) => {
         board[i] = value;
@@ -16,6 +16,25 @@ const gameBoard = (() => {
 
 const displayController = (() => {
     const board = gameBoard.getBoard();
+    const cells = document.querySelectorAll(".cell");
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] !== "") {
+            cells[i].textContent = board[i];
+        }
+        
+    }
     
-    
+})();
+
+const player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    return { getName, getSymbol };
+}
+
+const player1 = player("Player 1", "X");
+const player2 = player("Player 2", "O");
+
+const game = (() => {
+
 })();
